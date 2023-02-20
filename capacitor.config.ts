6890b2e514b5d04f5c55ceb0494a686004c9ac81
arwe <94793167+qapris@users.noreply.github.com>
2023-02-20 15:33:19 +0300

    added live update sdk

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'material.dashboard.2.react',
+  appName: 'material-dashboard-2-react',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
